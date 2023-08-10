941367ea9f0223ed2ece4a781f2c35e1c9eab50e
viggoStrom <104303404+viggoStrom@users.noreply.github.com>
2023-08-11 00:47:47 +0200

    mvp backend

---
+++ count.py
@@ -0,0 +1,2 @@
+with open("links.csv","r") as file:
+    print(file.readline().count(","))
--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import puppeteer from "puppeteer";
 import fs from "fs"
 
 
-const getNames = async (url) => {
+const scrape = async (urlParam) => {
+    let url = urlParam
+
     const browser = await puppeteer.launch({
         headless: false,
         defaultViewport: null,
@@ -15,51 +17,71 @@ const getNames = async (url) => {
         waitUntil: "domcontentloaded",
     });
 
-    const listOfLinks = await page.evaluate(() => {
+    let flag = true
+    while (flag) {
+        const listOfLinks = await page.evaluate(() => {
 
-        const nodeElements = document.querySelectorAll(".category-page__member-link");
+            const nodeElements = document.querySelectorAll(".category-page__member-link");
 
-        let links = []
-        nodeElements.forEach(node => {
-            links.push(node.href)
-        })
+            let links = []
+            nodeElements.forEach(node => {
+                links.push(node.href)
+            })
 
-        return links;
-    });
+            try {
+                url = document.querySelector(".category-page__pagination-next").href
+            } catch (err) {
+                url = ""
+            }
 
-    // console.log(listOfLinks);
-    // let alternatives = []
-    // let randomChar = listOfLinks[Math.floor(Math.random() * listOfLinks.length)]
-    // let i = 0
-    // while (randomChar.includes() == false && i < 3) {
-    //     randomChar = listOfLinks[Math.floor(Math.random() * listOfLinks.length)]
-    //     i++
-    //     alternatives.push(randomChar)
-    // }
+            return { links, url };
+        });
+        
 
-    const content = JSON.stringify(listOfLinks).replaceAll("https://starwars.fandom.com/wiki/", "").replace("[", "").replace("]", "") + ","
+        // console.log(listOfLinks);
+        // let alternatives = []
+        // let randomChar = listOfLinks[Math.floor(Math.random() * listOfLinks.length)]
+        // let i = 0
+        // while (randomChar.includes() == false && i < 3) {
+        //     randomChar = listOfLinks[Math.floor(Math.random() * listOfLinks.length)]
+        //     i++
+        //     alternatives.push(randomChar)
+        // }
 
-    try {
-        fs.writeFileSync("links.csv", content, { flag: "a" });
-        // file written successfully
-    } catch (err) {
-        console.error(err);
+        const content = JSON.stringify(listOfLinks.links).replaceAll("https://starwars.fandom.com/wiki/", "").replace("[", "").replace("]", "") + ","
+
+        try {
+            fs.writeFileSync("links.csv", content, { flag: "a" });
+            // file written successfully
+        } catch (err) {
+            console.error(err);
+        }
+
+        if (url = "") {
+            flag = false
+            break
+        }
+
+        await page.goto(listOfLinks.url, {
+            waitUntil: "domcontentloaded",
+        });
     }
 
     await browser.close();
 };
 
 
-// getNames("https://starwars.fandom.com/wiki/Category:Females")
-// getNames("https://starwars.fandom.com/wiki/Category:Unidentified_females")
-// getNames("https://starwars.fandom.com/wiki/Category:Droids_with_feminine_programming")
-getNames("https://starwars.fandom.com/wiki/Category:Males")
-// getNames("https://starwars.fandom.com/wiki/Category:Unidentified_males")
-// getNames("https://starwars.fandom.com/wiki/Category:Droids_with_masculine_programming")
-// getNames("https://starwars.fandom.com/wiki/Category:Non-binary_individuals")
-// getNames("https://starwars.fandom.com/wiki/Category:Trans_individuals")
-// getNames("https://starwars.fandom.com/wiki/Category:Genderless_individuals")
-// getNames("https://starwars.fandom.com/wiki/Category:Droids_with_no_gender_programming")
-// getNames("https://starwars.fandom.com/wiki/Category:Individuals_of_unspecified_gender")
-// getNames("https://starwars.fandom.com/wiki/Category:Droids_with_unspecified_gender_programming")
-// getNames("https://starwars.fandom.com/wiki/Category:Individuals_of_unidentified_gender")
+// scrape("https://starwars.fandom.com/wiki/Category:Males")
+scrape("https://starwars.fandom.com/wiki/Category:Females")
+
+// scrape("https://starwars.fandom.com/wiki/Category:Unidentified_females")
+// scrape("https://starwars.fandom.com/wiki/Category:Droids_with_feminine_programming")
+// scrape("https://starwars.fandom.com/wiki/Category:Unidentified_males")
+// scrape("https://starwars.fandom.com/wiki/Category:Droids_with_masculine_programming")
+// scrape("https://starwars.fandom.com/wiki/Category:Non-binary_individuals")
+// scrape("https://starwars.fandom.com/wiki/Category:Trans_individuals")
+// scrape("https://starwars.fandom.com/wiki/Category:Genderless_individuals")
+// scrape("https://starwars.fandom.com/wiki/Category:Droids_with_no_gender_programming")
+// scrape("https://starwars.fandom.com/wiki/Category:Individuals_of_unspecified_gender")
+// scrape("https://starwars.fandom.com/wiki/Category:Droids_with_unspecified_gender_programming")
+// scrape("https://starwars.fandom.com/wiki/Category:Individuals_of_unidentified_gender")
