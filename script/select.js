e4616bdf15709bac20f74981d9ad7aae5a65d788
viggoStrom <104303404+viggoStrom@users.noreply.github.com>
2023-08-12 22:51:42 +0200

    selection done

---
+++ script/select.js
@@ -0,0 +1,56 @@
+
+localStorage.clear()
+for (let index = 1; index <= 3; index++) {
+    localStorage.setItem("bed" + index.toString(), false)
+    localStorage.setItem("wed" + index.toString(), false)
+    localStorage.setItem("behead" + index.toString(), false)
+}
+
+const select = (srcElement, id) => {
+    if (srcElement.classList.contains("selected")) {
+        srcElement.classList.remove("selected")
+        localStorage.setItem(id, false)
+        return
+    }
+
+    if (id.includes("bed")) {
+        localStorage.setItem("bed1", false)
+        localStorage.setItem("bed2", false)
+        localStorage.setItem("bed3", false)
+        localStorage.setItem(id, true)
+    } else if (id.includes("wed")) {
+        localStorage.setItem("wed1", false)
+        localStorage.setItem("wed2", false)
+        localStorage.setItem("wed3", false)
+        localStorage.setItem(id, true)
+    } else if (id.includes("behead")) {
+        localStorage.setItem("behead1", false)
+        localStorage.setItem("behead2", false)
+        localStorage.setItem("behead3", false)
+        localStorage.setItem(id, true)
+    }
+    if (id.includes("1")) {
+        localStorage.setItem("bed1", false)
+        localStorage.setItem("wed1", false)
+        localStorage.setItem("behead1", false)
+        localStorage.setItem(id, true)
+    } else if (id.includes("2")) {
+        localStorage.setItem("wed2", false)
+        localStorage.setItem("bed2", false)
+        localStorage.setItem("behead2", false)
+        localStorage.setItem(id, true)
+    } else if (id.includes("3")) {
+        localStorage.setItem("bed3", false)
+        localStorage.setItem("wed3", false)
+        localStorage.setItem("behead3", false)
+        localStorage.setItem(id, true)
+    }
+
+    document.querySelectorAll(".iconWrapper div").forEach(element => {
+        if (localStorage.getItem(element.id) == "false") {
+            element.classList.remove("selected")
+        } else {
+            element.classList.add("selected")
+        }
+    })
+}
