cc159fe454f3bda9727be0a250fa71c7e6e878b0
viggoStrom <104303404+viggoStrom@users.noreply.github.com>
2023-08-14 17:34:44 +0200

    figuring out comms

--- script/readCharacter.js
+++ script/readCharacter.js
@@ -18,27 +18,27 @@ fetch("dataVault/filteredOutput.csv")
 
         console.log(alternatives);
 
-        async function getNameAndImage(title) {
+        async function updateImageAndName(element, title) {
             try {
-                const response = await fetch(`https://viggostrom.github.io/bedWedBehead/backend.js:3000/getPage/${title}`);
-                // const response = await fetch(`http://localhost:3000/getPage/${title}`);
-                const data = await response.json();
-                return data;
+                element.href = "https://starwars.fandom.com/wiki/" + title
+                
+                console.log("Request sent");
+                const response = await fetch(`http://localhost:3000/getPage/${title}`);
+                const result = await response.json();
+                console.log("Request fulfilled");
+
+                element.children[0].src = result.imageURL
+                element.children[1].innerHTML = result.name
             } catch (error) {
                 console.error(error);
                 return { title: "Error", imageURL: "" };
             }
         }
-
+        
         const headshots = document.querySelectorAll(".imageWrapper")
         headshots.forEach((element, index) => {
             const title = alternatives[index]
-            const result = getNameAndImage(title)
-            console.log(result);
-            const URL = "https://starwars.fandom.com/wiki/" + title
-            element.href = URL
-            element.children[0].src = result.imageURL
-            element.children[1].innerHTML = result.name
+            updateImageAndName(element, title)
         })
     })
     .catch((error) => console.error(error))
