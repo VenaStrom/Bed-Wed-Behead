ef430911597330b1b9fe3c9ac3072ef4253d4a2c
viggoStrom <104303404+viggoStrom@users.noreply.github.com>
2023-08-14 14:41:31 +0200

    changed address

--- backend.js
+++ backend.js
@@ -60,7 +60,7 @@ import cors from "cors"
 const app = express();
 const port = 3000;
 
-app.use(cors({ origin: "http://127.0.0.1:5500" }))
+app.use(cors({ origin: "https://viggostrom.github.io/bedWedBehead/" }))
 // app.use(cors({ origin: "http://127.0.0.1:5500" }))
 
 app.get('/getPage/:title', async (req, res) => {
