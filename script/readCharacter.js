aebc147331eda7e15a866155401a86a2c78e3fba
viggoStrom <104303404+viggoStrom@users.noreply.github.com>
2023-08-26 21:59:49 +0200

    REAL MVP! :)

--- script/readCharacter.js
+++ script/readCharacter.js
@@ -54,63 +54,64 @@ fetch("dataVault/filteredOutput.csv")
                     imageWrapper.children[0].src = data.imageURL;
                     imageWrapper.children[1].innerHTML = data.name;
                 })
+
+                .catch((error) => {
+                    // Read local pre-indexed list
+                    fetch("./dataVault/indexedList.json")
+                        .then((result) => result.text())
+                        .then((text) => {
+                            indexedList = JSON.parse(text)
+
+                            const getRandomName = (checkAgainst) => {
+                                const keys = Object.keys(indexedList)
+                                let uriName = keys[Math.floor(Math.random() * keys.length)]
+                                while (checkAgainst.includes(uriName)) {
+                                    uriName = keys[Math.floor(Math.random() * keys.length)];
+                                }
+
+                                return uriName
+                            }
+
+                            let alternatives = [];
+
+                            while (alternatives.length < 3) {
+                                alternatives.push(getRandomName(alternatives))
+                            }
+
+                            console.log(alternatives);
+
+
+                            const setCharacter = async (uriName, index) => {
+                                const data = indexedList[uriName]
+
+                                // Filters
+                                try {
+                                    if (data.imageURL == "" && !filter.image) {
+                                        data.imageURL = "style/images/placeholder-alien.png"
+                                    } else if (data.imageURL == "" && filter.image) {
+                                        return setCharacter(getRandomName(alternatives), index)
+                                    }
+                                } catch (error) {
+
+                                }
+
+                                console.log(index, uriName);
+
+                                const imageWrapper = document.querySelectorAll(".imageWrapper")[index];
+
+                                const URL = "https://starwars.fandom.com/wiki/" + uriName;
+                                imageWrapper.href = URL;
+                                imageWrapper.children[0].src = data.imageURL;
+                                imageWrapper.children[1].innerHTML = data.name;
+                            }
+
+
+                            alternatives.forEach((uriName, index) => { setCharacter(uriName, index) })
+                        })
+
+                })
         }
 
         alternatives.forEach((uriName, index) => setCharacter(uriName, index))
-
-    })
-    .catch((error) => {
-        // Read local pre-indexed list
-        fetch("./dataVault/indexedList.json")
-            .then((result) => result.text())
-            .then((text) => {
-                indexedList = JSON.parse(text)
-
-                const getRandomName = (checkAgainst) => {
-                    const keys = Object.keys(indexedList)
-                    let uriName = keys[Math.floor(Math.random() * keys.length)]
-                    while (checkAgainst.includes(uriName)) {
-                        uriName = keys[Math.floor(Math.random() * keys.length)];
-                    }
-
-                    return uriName
-                }
-
-                let alternatives = [];
-
-                while (alternatives.length < 3) {
-                    alternatives.push(getRandomName(alternatives))
-                }
-
-                console.log(alternatives);
-
-
-                const setCharacter = async (uriName, index) => {
-                    const data = indexedList[uriName]
-
-                    // Filters
-                    try {
-                        if (data.imageURL == "" && !filter.image) {
-                            data.imageURL = "style/images/placeholder-alien.png"
-                        } else if (data.imageURL == "" && filter.image) {
-                            return setCharacter(getRandomName(alternatives), index)
-                        }
-                    } catch (error) {
-
-                    }
-
-                    console.log(index, uriName);
-
-                    const imageWrapper = document.querySelectorAll(".imageWrapper")[index];
-
-                    const URL = "https://starwars.fandom.com/wiki/" + uriName;
-                    imageWrapper.href = URL;
-                    imageWrapper.children[0].src = data.imageURL;
-                    imageWrapper.children[1].innerHTML = data.name;
-                }
-
-
-                alternatives.forEach((uriName, index) => { setCharacter(uriName, index) })
-            })
 
     })
