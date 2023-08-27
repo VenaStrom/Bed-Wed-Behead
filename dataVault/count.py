e516d36b11c04e98ae9bf840dd1752a936e1c37d
viggoStrom <104303404+viggoStrom@users.noreply.github.com>
2023-08-27 22:37:25 +0200

    No dupes vehicles

--- dataVault/count.py
+++ dataVault/count.py
@@ -1,3 +1,8 @@
 
-with open("dataVault/ships/rawScrapeOutput.csv", "r") as file:
-    print(file.read().count(","))
+with open("dataVault/ships/noDupes.csv", "r") as file:
+    data = file.read()
+    length = data.count(",") + 1
+    unidentified = data.count("Unidentified")
+    print("Length: " + str(length))
+    print("Unidentified: " + str(unidentified))
+    print("Ratio: " + str(unidentified/length))
