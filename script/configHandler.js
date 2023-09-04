5f49bdacdebe8342e2ed5a2fe0b59819495ab32f
viggoStrom <104303404+viggoStrom@users.noreply.github.com>
2023-09-04 11:00:34 +0200

    config stuff

--- script/configHandler.js
+++ script/configHandler.js
@@ -1,33 +1,45 @@
 
 
+const defineConfigOption = (name) => {
+    if (window.localStorage.getItem("config") == null) {
+        consolce.log("localStorage is missing");
+
+        window.localStorage.setItem(name, "unchecked")
+    }
+    else if (window.localStorage.getItem(name) == "checked") {
+        const checkbox = document.getElementById(name)
+        checkbox.checked = true
+    } if (window.localStorage.getItem(name).includes("TextInput") == "text") {
+        const textBox = document.getElementById(name)
+        textBox.value = window.localStorage.getItem(name)
+    }
+}
+
+
 window.onload = (event) => {
 
     // Define local storage variables if not present
     if (window.localStorage.getItem("config") == null) {
-
-        // if (window.localStorage.getItem("config") != "open" && window.localStorage.getItem("config") != "closed") {
-        console.log("localStorage is missing");
+        console.error("Config reopen error: localStorage is missing");
         window.localStorage.setItem("config", "closed")
-
-        // Add items here when you make another config
-        window.localStorage.setItem("filterImage", "unchecked")
-        window.localStorage.setItem("filterString", "unchecked")
     }
-    
-    // Implement relevant config options
+
+    // Add items here when you make another config
+    defineConfigOption("filterImage")
+    defineConfigOption("filterString")
+    defineConfigOption("filterUnidentified")
+
+    // Keep config open if necessary
     if (window.localStorage.getItem("config") == "open") {
         const configMenu = document.getElementById("configMenu");
-        configMenu.style.display = "grid";
+        configMenu.style.display = "block";
         window.localStorage.setItem("config", "open")
     }
-    if (window.localStorage.getItem("filterImage") == "checked"){
-        const checkbox = document.getElementById("filterImage")
-        checkbox.checked = true
-    }
-    if (window.localStorage.getItem("filterString") == "checked"){
-        const checkbox = document.getElementById("filterString")
-        checkbox.checked = true
-    }
+
+    // Add the toggle config function to the inputs
+    // document.querySelectorAll("#configForm input[type=checkbox]").forEach(input => {
+    //     input.onchange = "toggleConfig(this)"
+    // });
 }
 
 
@@ -36,7 +48,7 @@ const toggleConfigMenu = () => {
     const configMenu = document.getElementById("configMenu");
 
     if (configMenu.style.display == "none") {
-        configMenu.style.display = "grid";
+        configMenu.style.display = "block";
         window.localStorage.setItem("config", "open")
     } else {
         configMenu.style.display = "none";
@@ -45,28 +57,7 @@ const toggleConfigMenu = () => {
 }
 
 
-// Closes the config menu if you press esc (or click outside of menu)
-document.addEventListener("keydown", (event) => {
-    if (window.localStorage.getItem("config") == "open" && event.key == "Escape") {
-        toggleConfigMenu()
-    }
-})
-document.addEventListener("mousedown", (event) => {
-    if (window.localStorage.getItem("config") == "open") {
-        console.log(event.target);
-        try {
-            if (
-                event.target.id != "configMenu" &&
-                !event.target.classList.includes("configCheckbox") &&
-                !event.target.classList.includes("configName") 
-            ) {
-                toggleConfigMenu()
-            }
-        } catch (error) { }
-    }
-})
-
-
+// Toggle a single config option
 const toggleConfig = (srcElement) => {
     const configId = srcElement.id
 
@@ -76,3 +67,52 @@ const toggleConfig = (srcElement) => {
         window.localStorage.setItem(configId, "unchecked")
     }
 }
+
+const updateConfig = () => {
+    const configForm = document.forms["configForm"]
+    const configInputs = {
+        filterImage: configForm["filterImage"],
+        filterUnidentified: configForm["filterUnidentified"],
+        filterStringTextInput: configForm["filterStringTextInput"],
+        filterString: configForm["filterString"],
+    }
+
+    Object.keys(configInputs).forEach(key => {
+
+        const element = configInputs[key]
+
+        if (element.type == "text") {
+            // For the text field
+            window.localStorage.setItem(key, element.value)
+        } else if (element.checked) {
+            window.localStorage.setItem(key, "checked")
+        } else if (!element.checked) {
+            window.localStorage.setItem(key, "unchecked")
+        }
+    })
+}
+
+
+// Closes the config menu if you press esc or click outside of menu
+document.addEventListener("keydown", (event) => {
+    if (window.localStorage.getItem("config") == "open" && event.key == "Escape") {
+        toggleConfigMenu()
+    }
+})
+// document.addEventListener("mousedown", (event) => {
+//     if (window.localStorage.getItem("config") == "open") {
+//         try {
+//             if (
+//                 event.target.nodeName != "H2" &&
+//                 event.target.id != "gearIcon" &&
+//                 event.target.id != "configMenu" &&
+//                 event.target.id != "configForm" &&
+//                 !event.target.classList.contains("configName") &&
+//                 !event.target.classList.contains("configTextInput") &&
+//                 !event.target.classList.contains("configCheckbox")
+//             ) {
+//                 toggleConfigMenu()
+//             }
+//         } catch (error) { }
+//     }
+// })
--- script/readCharacter.js
+++ script/readCharacter.js
@@ -1,5 +1,4 @@
 
-
 const URLs = {
     // noDupes: "dataVault/ships/noDupes.csv",
     noDupes: "dataVault/individuals/noDupes.csv",
@@ -8,123 +7,124 @@ const URLs = {
 }
 
 
-fetch(URLs.noDupes)
-    .then((result) => result.text())
-    .then((text) => {
-        document.querySelectorAll(".imageWrapper p").forEach(title => {
-            title.innerHTML = "Fetching"
-        });
 
-        const fullListOfIndividuals = text.replaceAll("\"", "").replaceAll(" ", "").split(",")
+const play = () => {
+    fetch(URLs.noDupes)
+        .then((result) => result.text())
+        .then((text) => {
+            document.querySelectorAll(".imageWrapper p").forEach(title => {
+                title.innerHTML = "Fetching"
+            });
 
-        document.getElementById("totalCharacterCount").innerHTML = fullListOfIndividuals.length - 1
+            const fullListOfIndividuals = text.replaceAll("\"", "").replaceAll(" ", "").split(",")
 
-        const getRandomName = (checkAgainst) => {
-            const names = fullListOfIndividuals
-            let uriName = names[Math.floor(Math.random() * names.length)]
-            while (checkAgainst.includes(uriName)) {
-                uriName = names[Math.floor(Math.random() * names.length)];
+            document.getElementById("totalCharacterCount").innerHTML = fullListOfIndividuals.length - 1
+
+            const getRandomName = (checkAgainst) => {
+                const names = fullListOfIndividuals
+                let uriName = names[Math.floor(Math.random() * names.length)]
+                while (checkAgainst.includes(uriName)) {
+                    uriName = names[Math.floor(Math.random() * names.length)];
+                }
+
+                return uriName
             }
 
-            return uriName
-        }
+            let alternatives = [];
 
-        let alternatives = [];
+            while (alternatives.length < 3) {
+                alternatives.push(getRandomName(alternatives))
+            }
 
-        while (alternatives.length < 3) {
-            alternatives.push(getRandomName(alternatives))
-        }
+            console.log(alternatives);
 
-        console.log(alternatives);
+            const setCharacter = async (uriName, index) => {
+                fetch(`http://localhost:3000/api/getPage/${uriName}`)
+                    .then((response) => response.json())
+                    .then((data) => {
 
+                        // Filters
+                        if (data.imageURL == "" && window.localStorage.getItem("filterImage") == "unchecked") {
+                            data.imageURL = URLs.placeholderImage
+                        } else if (data.imageURL == "" && window.localStorage.getItem("filterImage") == "checked") {
+                            return setCharacter(getRandomName(alternatives), index)
+                        }
 
-        const setCharacter = async (uriName, index) => {
-            fetch(`http://localhost:3000/api/getPage/${uriName}`)
-                .then((response) => response.json())
-                .then((data) => {
+                        console.log(index, data);
 
-                    // Filters
-                    if (data.imageURL == "" && window.localStorage.getItem("filterImage") == "unchecked") {
-                        data.imageURL = URLs.placeholderImage
-                    } else if (data.imageURL == "" && window.localStorage.getItem("filterImage") == "checked") {
-                        return setCharacter(getRandomName(alternatives), index)
-                    }
+                        const imageWrapper = document.querySelectorAll(".imageWrapper")[index];
 
+                        const URL = "https://starwars.fandom.com/wiki/" + uriName;
+                        imageWrapper.href = URL;
+                        imageWrapper.children[0].src = data.imageURL;
+                        imageWrapper.children[1].innerHTML = data.name;
 
-                    console.log(index, data);
+                        // Kinda janky solution but it works for now
+                        setCharacter.breakFlag = true
+                    })
+                    .catch((error) => {
 
-                    const imageWrapper = document.querySelectorAll(".imageWrapper")[index];
-
-                    const URL = "https://starwars.fandom.com/wiki/" + uriName;
-                    imageWrapper.href = URL;
-                    imageWrapper.children[0].src = data.imageURL;
-                    imageWrapper.children[1].innerHTML = data.name;
-
-                    // Kinda janky solution but it works for now
-                    setCharacter.breakFlag = true
-                })
-                .catch((error) => {
-                    // Kinda janky solution but it works for now
-                    if (setCharacter.breakFlag) {
-                        return
-                    }
-                    setCharacter.breakFlag = true
-
-                    // Read local pre-indexed list
-                    fetch(URLs.preIndexed)
-                        .then((result) => result.text())
-                        .then((text) => {
-                            indexedList = JSON.parse(text)
-
-                            const getRandomName = (checkAgainst) => {
-                                const keys = Object.keys(indexedList)
-                                let uriName = keys[Math.floor(Math.random() * keys.length)]
-                                while (checkAgainst.includes(uriName)) {
-                                    uriName = keys[Math.floor(Math.random() * keys.length)];
-                                }
-
-                                return uriName
-                            }
-
-                            let alternatives = [];
-
-                            while (alternatives.length < 3) {
-                                alternatives.push(getRandomName(alternatives))
-                            }
-
-                            console.log(alternatives);
+                        // Kinda janky solution but it works for now
+                        if (setCharacter.breakFlag) {
+                            return
+                        } else {
+                            setCharacter.breakFlag = true
+                        }
 
 
-                            const setCharacter = async (uriName, index) => {
-                                const data = indexedList[uriName]
+                        // Read local pre-indexed list
+                        fetch(URLs.preIndexed)
+                            .then((result) => result.text())
+                            .then((text) => {
 
-                                // Filters
-                                try {
-                                    if (data.imageURL == "" && window.localStorage.getItem("filterImage") == "unchecked") {
-                                        data.imageURL = URLs.placeholderImage
-                                    } else if (data.imageURL == "" && window.localStorage.getItem("filterImage") == "checked") {
-                                        return setCharacter(getRandomName(alternatives), index)
+                                indexedList = JSON.parse(text)
+
+                                const getRandomName = (checkAgainst) => {
+                                    const keys = Object.keys(indexedList)
+                                    let uriName = keys[Math.floor(Math.random() * keys.length)]
+                                    while (checkAgainst.includes(uriName)) {
+                                        uriName = keys[Math.floor(Math.random() * keys.length)];
                                     }
-                                } catch (error) {
 
+                                    return uriName
                                 }
 
-                                console.log(index, uriName);
+                                let alternatives = [];
 
-                                const imageWrapper = document.querySelectorAll(".imageWrapper")[index];
+                                while (alternatives.length < 3) {
+                                    alternatives.push(getRandomName(alternatives))
+                                }
 
-                                const URL = "https://starwars.fandom.com/wiki/" + uriName;
-                                imageWrapper.href = URL;
-                                imageWrapper.children[0].src = data.imageURL;
-                                imageWrapper.children[1].innerHTML = data.name;
-                            }
+                                console.log(alternatives);
 
 
-                            alternatives.forEach((uriName, index) => { setCharacter(uriName, index) })
-                        })
-                })
-        }
+                                const setCharacter = async (uriName, index) => {
+                                    let data = indexedList[uriName]
 
-        alternatives.forEach((uriName, index) => setCharacter(uriName, index))
+                                    // Filter
+                                    try {
+                                        if (data.imageURL == "" && window.localStorage.getItem("filterImage") == "unchecked") {
+                                            data.imageURL = URLs.placeholderImage
+                                        } else if (data.imageURL == "" && window.localStorage.getItem("filterImage") == "checked") {
+                                            return setCharacter(getRandomName(alternatives), index)
+                                        }
+                                    } catch (error) { }
 
-    })
+                                    console.log(index, uriName);
+
+                                    const imageWrapper = document.querySelectorAll(".imageWrapper")[index];
+
+                                    const URL = "https://starwars.fandom.com/wiki/" + uriName;
+                                    imageWrapper.href = URL;
+                                    imageWrapper.children[0].src = data.imageURL;
+                                    imageWrapper.children[1].innerHTML = data.name;
+                                }
+                                alternatives.forEach((uriName, index) => { setCharacter(uriName, index) })
+                            })
+                    })
+            }
+            alternatives.forEach((uriName, index) => setCharacter(uriName, index))
+        })
+}
+
+play()
