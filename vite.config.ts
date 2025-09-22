4353c1a842ef7321c4d50612b65ace36385dfa7a
Vena <strom.vena+gh@gmail.com>
2025-09-22 21:29:40 +0200

    move stuff

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig, UserConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  root: "src",
+  publicDir: "../public",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
+  define: {
+    "__BUILD_DATE__": JSON.stringify(new Date().toLocaleDateString("en-SE", { year: "numeric", month: "2-digit", day: "2-digit" })),
+  },
+}) satisfies UserConfig;
