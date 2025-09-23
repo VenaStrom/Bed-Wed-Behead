7edc070e6ae0aaa60bd3ff145e9edda4cf99f345
Vena <strom.vena+gh@gmail.com>
2025-09-24 00:35:16 +0200

    WIP scraping

---
+++ tools/minimize-character-links.ts
@@ -0,0 +1,31 @@
+import fs from "node:fs";
+
+const prunedCharacterLinksFile = "tools/out/character-links.pruned.json";
+const minimizeCharacterLinksFile = "tools/out/character-links.minimized.json";
+
+if (!fs.existsSync(prunedCharacterLinksFile)) {
+  console.error(`File not found: ${prunedCharacterLinksFile}`);
+  process.exit(1);
+}
+
+console.log("Minimizing character links...");
+
+const characterLinks: string[] = JSON.parse(fs.readFileSync(prunedCharacterLinksFile, "utf-8"));
+const minimizedLinks = characterLinks.map(link => link.replace("/wiki/", "")).sort();
+
+const singleLineLinks = minimizedLinks.join("?");
+
+// Self test. Splitting should give the same number of links.
+const testLinks = singleLineLinks.split("?");
+if (testLinks.length !== minimizedLinks.length) {
+  console.error("Error: Mismatch in link counts after minimization.");
+  console.log(`Original count: ${minimizedLinks.length}, After split count: ${testLinks.length}`);
+  process.exit(1);
+}
+
+// Log size
+console.log(`Original size: ${JSON.stringify(characterLinks, null, 2).length} characters`);
+console.log(`Minimized size: ${singleLineLinks.length} characters`);
+console.log(`~ ${singleLineLinks.length / 1024} KB`);
+
+fs.writeFileSync(minimizeCharacterLinksFile, JSON.stringify({ singleLineData: singleLineLinks }));
---
+++ tools/prune-character-links.ts
@@ -0,0 +1,26 @@
+import fs from "node:fs";
+
+const characterLinksFile = "tools/out/character-links.json";
+const prunedCharacterLinksFile = "tools/out/character-links.pruned.json";
+
+if (!fs.existsSync(characterLinksFile)) {
+  console.error(`File not found: ${characterLinksFile}`);
+  process.exit(1);
+}
+
+console.log("Pruning character links...");
+
+const characterLinks: string[] = JSON.parse(fs.readFileSync(characterLinksFile, "utf-8"));
+const uniqueLinks = Array.from(new Set(characterLinks)).sort();
+
+// How many dupes?
+const dupesCount = characterLinks.length - uniqueLinks.length;
+if (dupesCount > 0) {
+  console.log(`Removed ${dupesCount} duplicate links.`);
+} else {
+  console.log("No duplicate links found.");
+}
+
+fs.writeFileSync(prunedCharacterLinksFile, JSON.stringify(uniqueLinks, null, 2));
+console.log(`Saved pruned character links to ${prunedCharacterLinksFile}`);
+console.log(`Total unique character links: ${uniqueLinks.length}`);
