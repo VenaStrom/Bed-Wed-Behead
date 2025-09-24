23874d2c6f536e82ef69c877ace3d43aee197751
Vena <strom.vena+gh@gmail.com>
2025-09-24 11:38:48 +0200

    WIP character data extraction

--- src/types.ts
+++ src/types.ts
@@ -13,8 +13,8 @@ export type ProfileState = {
  */
 export type Character = {
   n: string;
-  // Only the path part of the URL, e.g. Luke_Skywalker excluding https://starwars.fandom.com/wiki/
+  // Only the path part of the URL, e.g. Luke_Skywalker excluding "https://starwars.fandom.com/wiki/"
   r: string;
-  i?: string | undefined; // Omit if not present for size optimization
+  i?: string | undefined; // Omit if not present for size optimization. Only the route part of the URL. Excludes "https://static.wikia.nocookie.net/starwars/images/"
   c?: string[] | undefined;
 };
--- tools/scrape/extract-characters.ts
+++ tools/scrape/extract-characters.ts
@@ -1,6 +1,16 @@
 import fs from "node:fs";
 import { JSDOM } from "jsdom";
 import type { Character } from "../../src/types.ts";
+import Crypto from "node:crypto";
+
+function catHash(input: string): string {
+  const hash = Crypto.createHash("sha256");
+  const data = hash.update(input, "utf-8");
+  const digest = data.digest("hex");
+  const truncated = digest.slice(0, 8); // First 8 bytes = 64 bits
+
+  return truncated
+}
 
 const characterLinksPath = "tools/out/character-links.pruned.json";
 if (!fs.existsSync(characterLinksPath)) {
@@ -9,6 +19,7 @@ if (!fs.existsSync(characterLinksPath)) {
 const characterLinks: string[] = JSON.parse(fs.readFileSync(characterLinksPath, "utf-8"));
 
 const baseURL = "https://starwars.fandom.com/wiki/";
+const imageBaseURL = "https://static.wikia.nocookie.net/starwars/images/";
 
 // Ensure output directory exists
 const cacheFolder = "tools/cache/characters";
@@ -20,9 +31,15 @@ if (!fs.existsSync(outFolder)) {
   fs.mkdirSync(outFolder, { recursive: true });
 }
 const charactersFile = `${outFolder}/characters.json`;
+const categoriesFile = `${outFolder}/categories.json`;
 
+// route: Character data
 const characters: Record<string, Character> = {};
-const categories: string[] = [];
+
+/** Hash: category name */
+const categoriesLookup: Record<string, string> = {};
+/** Name: hash */
+const categoriesLookupReverse: Record<string, string> = {};
 
 for (const route of characterLinks) {
   const link = baseURL + route;
@@ -50,11 +67,52 @@ for (const route of characterLinks) {
     console.warn(`No name found for ${link}`);
     continue;
   }
-  const categories = Array.from(document.querySelectorAll("li.category.normal"))
+  const categoryNames = Array.from(document.querySelectorAll("li.category.normal"))
     .map(el => el as HTMLLIElement)
     .map(e => e.dataset.name || null)
-    .filter(Boolean);
-  console.log(categories);
+    .filter(Boolean) as string[];
+
+  for (const name of categoryNames) {
+    const categoryHash = catHash(name);
+    if (!categoriesLookup[categoryHash]) {
+      categoriesLookup[categoryHash] = name;
+      categoriesLookupReverse[name] = categoryHash;
+    }
+    else {
+      console.log("WTF, hash collision?", name, categoriesLookup[categoryHash]);
+      throw new Error("Hash collision");
+    }
+  }
+  const categoryHashes = categoryNames.map(c => categoriesLookupReverse[c]);
+
+  let imageURL = document.head.querySelector("meta[property='og:image']")?.getAttribute("content")
+    || document.querySelector("img.pi-image-thumbnail")?.getAttribute("src")
+    || null;
+
+  if (imageURL) {
+    const cleanedImageURL = new URL(imageURL);
+    cleanedImageURL.searchParams.delete("cb");
+    const minifiedURL = cleanedImageURL.href.replace(imageBaseURL, "");
+    imageURL = minifiedURL;
+  }
+
+  if (characters[route]) {
+    // This should really never happen
+    console.warn(`Duplicate character found: ${route} (${name})`);
+    throw new Error("Duplicate character " + route);
+  }
+
+  // Compile character
+  characters[route] = {
+    // Pretty name
+    n: name,
+    // Route
+    r: route,
+    // Categories
+    ...(categoryNames.length > 0 ? { c: categoryHashes } : {}),
+    // Image URL
+    ...(imageURL ? { i: imageURL } : {}),
+  }
 
   // Partial write
   fs.writeFileSync(charactersFile, JSON.stringify(characters, null, 2));
