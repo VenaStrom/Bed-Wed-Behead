09a1ea3146b8d9d5da5280e87fb6c68428bdcd6a
Vena <strom.vena+gh@gmail.com>
2025-09-24 11:03:37 +0200

    WIP character data extraction

--- src/types.ts
+++ src/types.ts
@@ -6,13 +6,15 @@ export type ProfileState = {
 };
 
 /** 
- * `n`: name
+ * `n`: pretty name
  * `r`: wiki route
  * `i`: image link (optional)
+ * `c`: categories
  */
 export type Character = {
   n: string;
   // Only the path part of the URL, e.g. Luke_Skywalker excluding https://starwars.fandom.com/wiki/
   r: string;
   i?: string | undefined; // Omit if not present for size optimization
+  c?: string[] | undefined;
 };
--- tools/scrape/extract-characters.ts
+++ tools/scrape/extract-characters.ts
@@ -1,6 +1,6 @@
 import fs from "node:fs";
 import { JSDOM } from "jsdom";
-import { Character } from "../types.ts";
+import type { Character } from "../../src/types.ts";
 
 const characterLinksPath = "tools/out/character-links.pruned.json";
 if (!fs.existsSync(characterLinksPath)) {
@@ -22,9 +22,39 @@ if (!fs.existsSync(outFolder)) {
 const charactersFile = `${outFolder}/characters.json`;
 
 const characters: Record<string, Character> = {};
+const categories: string[] = [];
+
 for (const route of characterLinks) {
   const link = baseURL + route;
-  console.log(link);
+
+  // If in cache, load from there
+  if (!fs.existsSync(`${cacheFolder}/${route}.html`)) {
+    // Scrape and save
+    const response = await fetch(link);
+    if (!response.ok) {
+      console.warn(`Failed to fetch ${link}: ${response.statusText}`);
+      continue;
+    }
+    const text = await response.text();
+    fs.writeFileSync(`${cacheFolder}/${route}.html`, text);
+    console.log(`Saved ${route}.html`);
+  }
+
+  // Parse and extract
+  const html = fs.readFileSync(`${cacheFolder}/${route}.html`, "utf-8");
+  const dom = new JSDOM(html);
+  const document = dom.window.document;
+
+  const name = document.querySelector(".page-header__title")?.textContent?.trim() || null;
+  if (!name) {
+    console.warn(`No name found for ${link}`);
+    continue;
+  }
+  const categories = Array.from(document.querySelectorAll("li.category.normal"))
+    .map(el => el as HTMLLIElement)
+    .map(e => e.dataset.name || null)
+    .filter(Boolean);
+  console.log(categories);
 
   // Partial write
   fs.writeFileSync(charactersFile, JSON.stringify(characters, null, 2));
