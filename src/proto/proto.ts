2e454c3d443bb28c082ef77909cbf7f84c0495ae
Vena <strom.vena+gh@gmail.com>
2025-09-26 12:10:05 +0200

    base64 encode protobuf version of character data

--- src/proto/proto.ts
+++ src/proto/proto.ts
@@ -3,7 +3,7 @@ import type { Character } from "../types.ts";
 
 const protoPath = "src/proto/message.proto";
 
-export async function protoEncode(characters: Character[]): Promise<Uint8Array> {
+export async function protoEncode(characters: { characters: Character[] }): Promise<Uint8Array> {
   const root = await protobuf.load(protoPath);
   const dataStruct = root.lookupType("CharacterList");
   const formattedData = dataStruct.create(characters);
@@ -11,9 +11,9 @@ export async function protoEncode(characters: Character[]): Promise<Uint8Array> 
   return buffer;
 }
 
-export async function protoDecode(buffer: Buffer): Promise<protobuf.Message<object>> {
+export async function protoDecode(buffer: Uint8Array<ArrayBufferLike>): Promise<Character[]> {
   const root = await protobuf.load(protoPath);
   const dataStruct = root.lookupType("CharacterList");
-  const data = dataStruct.decode(buffer);
+  const data = dataStruct.decode(buffer).toJSON().characters as Character[];
   return data;
 }
--- tools/character-meta/minimize-characters.ts
+++ tools/character-meta/minimize-characters.ts
@@ -1,11 +1,14 @@
 import fs from "node:fs";
 import type { Character } from "../../src/types.ts";
 import { protoEncode } from "../../src/proto/proto.ts";
-import { } from "../../src/proto/generated.js";
 
 const characters: Character[] = JSON.parse(fs.readFileSync("tools/out/characters.raw.json", "utf-8"));
 const outPath = "tools/out/characters.min.json";
 
-const encodedCharacters = await protoEncode(characters);
-console.log(encodedCharacters.length);
-fs.writeFileSync(outPath, JSON.stringify({ meta: "Characters encoded with protobuf", bin: encodedCharacters.toString() }, null, 2), "utf-8");
+const encodedCharacters = await protoEncode({ characters });
+const base64 = Buffer.from(encodedCharacters).toString("base64");
+fs.writeFileSync(outPath, "\"" + base64 + "\"", "utf-8");
+
+// Decode like this
+// const decoded = (await protoDecode(encodedCharacters));
+// console.dir(decoded, { depth: null });
