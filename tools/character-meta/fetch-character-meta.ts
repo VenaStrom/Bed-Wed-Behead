e083ac5e3cb131d62378c78da398ba7ea6762b22
Vena <strom.vena+gh@gmail.com>
2025-10-02 22:32:37 +0200

    update api response type to be more manual and total rework of meta fetcher

--- tools/character-meta/fetch-character-meta.ts
+++ tools/character-meta/fetch-character-meta.ts
@@ -2,7 +2,161 @@ import fs from "node:fs";
 import type { Character } from "../../src/types.ts";
 import Crypto from "node:crypto";
 import { stdout } from "node:process";
-import type { MWPageParseResponse } from "../types.ts";
+import type { MWParsePageResponse } from "../types.ts";
+
+const apiBaseURL = "https://starwars.fandom.com/api.php?format=json&action=parse&page=";
+
+const characterLinksPath = "tools/out/character-links.pruned.json";
+if (!fs.existsSync(characterLinksPath)) {
+  throw new Error(`File not found: ${characterLinksPath}`);
+}
+const characterLinks: string[] = JSON.parse(fs.readFileSync(characterLinksPath, "utf-8"));
+
+const apiResponseCacheFolder = "tools/cache/api-response";
+if (!fs.existsSync(apiResponseCacheFolder)) {
+  fs.mkdirSync(apiResponseCacheFolder, { recursive: true });
+}
+const allNamesInCache = fs.readdirSync(apiResponseCacheFolder);
+
+const domCacheFolder = "tools/cache/dom";
+if (!fs.existsSync(domCacheFolder)) {
+  fs.mkdirSync(domCacheFolder, { recursive: true });
+}
+const allDomsInCache = fs.readdirSync(domCacheFolder);
+
+async function fetchAndSaveCharacterDataToFile(characterName: string) {
+  const fsSafeName = characterName.replaceAll("/", "_").replaceAll("\\", "_");
+  const cachePath = `${apiResponseCacheFolder}/${fsSafeName}.json`;
+
+  let apiResponse: MWParsePageResponse | null = null;
+
+  if (allNamesInCache.includes(`${fsSafeName}.json`) && allDomsInCache.includes(`${fsSafeName}.html`)) {
+    // Read from cache
+    const cachedData = fs.readFileSync(cachePath, "utf-8");
+    apiResponse = JSON.parse(cachedData) as MWParsePageResponse;
+    apiResponse.text = { "*": fs.readFileSync(`${domCacheFolder}/${fsSafeName}.html`, "utf-8") };
+  }
+  else {
+    // Fetch from API
+    const response = (await (await fetch(apiBaseURL + characterName, { headers: { "Accept-Encoding": "gzip" } })).json()).parse;
+    if (!response) {
+      console.log(`No data found for ${characterName}`);
+      return null;
+    }
+    apiResponse = response || null;
+  }
+
+  if (!apiResponse) {
+    console.log(`Somehow no data was retrieved for ${characterName}`);
+    return null;
+  }
+
+  // Save the dom separately
+
+  // Save to cache
+  if (!allNamesInCache.includes(`${fsSafeName}.json`)) fs.writeFileSync(cachePath, JSON.stringify(apiResponse));
+}
+
+const fetchPromises = characterLinks.slice(0, 1).map(name => fetchAndSaveCharacterDataToFile(name));
+// await Promise.all(fetchPromises);
+
+
+await fetchPromises[0]
+
+
+// const imageBaseURL = "https://static.wikia.nocookie.net/starwars/images/";
+// const characters: Character[] = [];
+// const categoryLookup: Record<string, string> = {}; // Hash: category name
+// const categoryLookupReverse: Record<string, string> = {}; // Category name: hash
+
+// async function saveCharacter(route: string) {
+//   const res = await fetchMetadata(route);
+//   if (!res) return null;
+
+//   const { name, categories, imageURL } = res;
+
+//   const categoryHashes = categories.map(categoryHash);
+//   for (const category of categories) {
+//     const hash = categoryHash(category);
+//     if (!categoryLookup[hash]) {
+//       categoryLookup[hash] = category;
+//       categoryLookupReverse[category] = hash;
+//     }
+//   }
+
+//   const character: Character = {
+//     name: name,
+//     route: route,
+//     ...(categoryHashes.length > 0 ? { categories: categoryHashes } : {}),
+//     ...(imageURL ? { image: imageURL.replace(imageBaseURL, "") } : {}),
+//   };
+
+//   characters.push(character);
+// }
+
+// const batchSize = 100;
+// const routeBatch: (() => Promise<void>)[] = new Array(Math.floor(characterLinks.length / batchSize))
+//   .fill(0)
+//   .map(() =>
+//     async () => {
+//       const batch = characterLinks.splice(0, batchSize);
+//       await Promise.all(batch.map(route => saveCharacter(route)));
+//     }
+//   );
+// // Push remaining links as the last batch
+// if (characterLinks.length > 0) {
+//   routeBatch.push(async () => {
+//     await Promise.all(characterLinks.map(route => saveCharacter(route)));
+//     characterLinks.splice(0, characterLinks.length); // Clear remaining links
+//   });
+// }
+// const totalBatches = routeBatch.length;
+
+// let i = -1;
+// let activeFetches = 0;
+// while (routeBatch.length > 0) {
+//   if (activeFetches >= 10) {
+//     await new Promise((resolve) => setTimeout(resolve, 50)); // Wait a little before retrying
+//     continue;
+//   }
+//   activeFetches++;
+
+//   const batchPromise = routeBatch.shift();
+//   if (!batchPromise) break;
+
+//   i++;
+//   const percentProgress = ((i / totalBatches) * 100).toFixed(2);
+
+//   stdout.write(`\n${percentProgress.padStart(6, " ")}% - "Batch ${i}"\t\t`);
+
+//   await batchPromise();
+
+//   activeFetches--;
+
+//   // Partial write after each batch
+//   fs.writeFileSync("tools/out/characters.raw.json", JSON.stringify(characters));
+//   fs.writeFileSync("tools/out/category-lookup.json", JSON.stringify(categoryLookup));
+//   fs.writeFileSync("tools/out/category-lookup-reverse.json", JSON.stringify(categoryLookupReverse));
+// }
+
+// console.log("Characters fetched:", characters.length);
+
+// fs.writeFileSync("tools/out/characters.raw.json", JSON.stringify(characters));
+// fs.writeFileSync("tools/out/category-lookup.json", JSON.stringify(categoryLookup));
+// fs.writeFileSync("tools/out/category-lookup-reverse.json", JSON.stringify(categoryLookupReverse));
+
+// process.on("beforeExit", (code) => {
+//   console.log("\nProcess exit, writing to files. Code:", code);
+//   fs.writeFileSync("tools/out/characters.raw.json", JSON.stringify(characters));
+//   fs.writeFileSync("tools/out/category-lookup.json", JSON.stringify(categoryLookup));
+//   fs.writeFileSync("tools/out/category-lookup-reverse.json", JSON.stringify(categoryLookupReverse));
+// });
+// process.on("exit", (code) => {
+//   console.log("\nProcess exit, writing to files. Code:", code);
+//   fs.writeFileSync("tools/out/characters.raw.json", JSON.stringify(characters));
+//   fs.writeFileSync("tools/out/category-lookup.json", JSON.stringify(categoryLookup));
+//   fs.writeFileSync("tools/out/category-lookup-reverse.json", JSON.stringify(categoryLookupReverse));
+// });
 
 function categoryHash(input: string): string {
   const hash = Crypto.createHash("sha256");
@@ -12,182 +166,3 @@ function categoryHash(input: string): string {
 
   return truncated;
 }
-
-const characterLinksPath = "tools/out/character-links.pruned.json";
-if (!fs.existsSync(characterLinksPath)) {
-  throw new Error(`File not found: ${characterLinksPath}`);
-}
-const characterLinks: string[] = JSON.parse(fs.readFileSync(characterLinksPath, "utf-8"));
-
-const cacheFolder = "tools/cache/characters";
-if (!fs.existsSync(cacheFolder)) {
-  fs.mkdirSync(cacheFolder, { recursive: true });
-}
-const categoryFetchCache = `tools/cache/fetched-categories`;
-if (!fs.existsSync(categoryFetchCache)) {
-  fs.mkdirSync(categoryFetchCache, { recursive: true });
-}
-const propertiesFetchCache = `tools/cache/fetched-properties`;
-if (!fs.existsSync(propertiesFetchCache)) {
-  fs.mkdirSync(propertiesFetchCache, { recursive: true });
-}
-
-async function fetchMetadata(uriEncodedName: string) {
-  const fsSafeName = uriEncodedName.replaceAll("/", "_").replaceAll("\\", "_");
-
-  const categoryCachePath = `${categoryFetchCache}/${fsSafeName}.json`;
-  const propertiesCachePath = `${propertiesFetchCache}/${fsSafeName}.json`;
-
-  const fetchers: (() => Promise<{ ok: boolean, json: () => Promise<{ parse: MWPageParseResponse }>, statusText: string }>)[] = [];
-  if (!fs.existsSync(categoryCachePath)) fetchers.push(async () => await fetch(`https://starwars.fandom.com/api.php?action=parse&page=${uriEncodedName}&prop=categories&format=json`));
-  else fetchers.push(async () => { return { ok: true, json: async () => JSON.parse(fs.readFileSync(categoryCachePath, "utf-8")), statusText: "From Cache" }; });
-  if (!fs.existsSync(propertiesCachePath)) fetchers.push(async () => await fetch(`https://starwars.fandom.com/api.php?action=parse&page=${uriEncodedName}&format=json&prop=properties`));
-  else fetchers.push(async () => { return { ok: true, json: async () => JSON.parse(fs.readFileSync(propertiesCachePath, "utf-8")), statusText: "From Cache" }; });
-
-  const [categoryRes, propertiesRes] = await Promise.all(fetchers.map(f => f()));
-
-  if (!categoryRes.ok) {
-    console.warn(`Failed to fetch categories for ${uriEncodedName}: ${categoryRes.statusText}`);
-    return null;
-  }
-  if (!propertiesRes.ok) {
-    console.warn(`Failed to fetch properties for ${uriEncodedName}: ${propertiesRes.statusText}`);
-    return null;
-  }
-
-  const categoryJSON = await categoryRes.json();
-  const propertiesJSON = await propertiesRes.json();
-
-  // Save fetched data to cache
-  if (!fs.existsSync(categoryCachePath) || !fs.existsSync(propertiesCachePath)) {
-    fs.writeFileSync(categoryCachePath, JSON.stringify(categoryJSON));
-    fs.writeFileSync(propertiesCachePath, JSON.stringify(propertiesJSON));
-  }
-
-  const categories: string[] = categoryJSON.parse.categories.map((cat: { "*": string }) => cat["*"].replaceAll("_", " "));
-  const hasInfobox = propertiesJSON.parse.properties.findIndex((prop: { name: string, "*": any }) => prop.name === "infoboxes") !== -1;
-  if (!hasInfobox) {
-    stdout.write(" ⚠️");
-    const name = categoryJSON.parse.title;
-    return {
-      name,
-      imageURL: null,
-      categories,
-    };
-  }
-
-  const infoboxData: { type: string, data: any }[] = JSON.parse(
-    propertiesJSON.parse.properties.find((prop: { name: string, "*": any }) => prop.name === "infoboxes")["*"],
-  ).at(0).data; // Go past the parser_tag_version wrapper
-
-  fs.writeFileSync(`tools/cache/infoboxes/${fsSafeName}.json`, JSON.stringify(infoboxData, null, 2));
-
-  // Prefer getting the name from the infobox title, as it's more likely to be correct than the page title
-  const name = infoboxData.find(box => box.type === "title")?.data?.value || categoryJSON.parse.title || null;
-  if (!name) {
-    console.warn(`No name found for ${uriEncodedName}`);
-    return null;
-  }
-
-  const imageURL: string | null = infoboxData.find(box => box.type === "image")?.data?.at(0)?.url || null;
-
-  return {
-    name,
-    imageURL,
-    categories,
-  };
-}
-
-const imageBaseURL = "https://static.wikia.nocookie.net/starwars/images/";
-const characters: Character[] = [];
-const categoryLookup: Record<string, string> = {}; // Hash: category name
-const categoryLookupReverse: Record<string, string> = {}; // Category name: hash
-
-async function saveCharacter(route: string) {
-  const res = await fetchMetadata(route);
-  if (!res) return null;
-
-  const { name, categories, imageURL } = res;
-
-  const categoryHashes = categories.map(categoryHash);
-  for (const category of categories) {
-    const hash = categoryHash(category);
-    if (!categoryLookup[hash]) {
-      categoryLookup[hash] = category;
-      categoryLookupReverse[category] = hash;
-    }
-  }
-
-  const character: Character = {
-    name: name,
-    route: route,
-    ...(categoryHashes.length > 0 ? { categories: categoryHashes } : {}),
-    ...(imageURL ? { image: imageURL.replace(imageBaseURL, "") } : {}),
-  };
-
-  characters.push(character);
-}
-
-const batchSize = 100;
-const routeBatch: (() => Promise<void>)[] = new Array(Math.floor(characterLinks.length / batchSize))
-  .fill(0)
-  .map(() =>
-    async () => {
-      const batch = characterLinks.splice(0, batchSize);
-      await Promise.all(batch.map(route => saveCharacter(route)));
-    }
-  );
-// Push remaining links as the last batch
-if (characterLinks.length > 0) {
-  routeBatch.push(async () => {
-    await Promise.all(characterLinks.map(route => saveCharacter(route)));
-    characterLinks.splice(0, characterLinks.length); // Clear remaining links
-  });
-}
-const totalBatches = routeBatch.length;
-
-let i = -1;
-let activeFetches = 0;
-while (routeBatch.length > 0) {
-  if (activeFetches >= 10) {
-    await new Promise((resolve) => setTimeout(resolve, 50)); // Wait a little before retrying
-    continue;
-  }
-  activeFetches++;
-
-  const batchPromise = routeBatch.shift();
-  if (!batchPromise) break;
-
-  i++;
-  const percentProgress = ((i / totalBatches) * 100).toFixed(2);
-
-  stdout.write(`\n${percentProgress.padStart(6, " ")}% - "Batch ${i}"\t\t`);
-
-  await batchPromise();
-
-  activeFetches--;
-
-  // Partial write after each batch
-  fs.writeFileSync("tools/out/characters.raw.json", JSON.stringify(characters));
-  fs.writeFileSync("tools/out/category-lookup.json", JSON.stringify(categoryLookup));
-  fs.writeFileSync("tools/out/category-lookup-reverse.json", JSON.stringify(categoryLookupReverse));
-}
-
-console.log("Characters fetched:", characters.length);
-
-fs.writeFileSync("tools/out/characters.raw.json", JSON.stringify(characters));
-fs.writeFileSync("tools/out/category-lookup.json", JSON.stringify(categoryLookup));
-fs.writeFileSync("tools/out/category-lookup-reverse.json", JSON.stringify(categoryLookupReverse));
-
-process.on("beforeExit", (code) => {
-  console.log("\nProcess exit, writing to files. Code:", code);
-  fs.writeFileSync("tools/out/characters.raw.json", JSON.stringify(characters));
-  fs.writeFileSync("tools/out/category-lookup.json", JSON.stringify(categoryLookup));
-  fs.writeFileSync("tools/out/category-lookup-reverse.json", JSON.stringify(categoryLookupReverse));
-});
-process.on("exit", (code) => {
-  console.log("\nProcess exit, writing to files. Code:", code);
-  fs.writeFileSync("tools/out/characters.raw.json", JSON.stringify(characters));
-  fs.writeFileSync("tools/out/category-lookup.json", JSON.stringify(categoryLookup));
-  fs.writeFileSync("tools/out/category-lookup-reverse.json", JSON.stringify(categoryLookupReverse));
-});
--- tools/types.ts
+++ tools/types.ts
@@ -1,2 +1,33 @@
-import type { ApiParseParams, } from "types-mediawiki-api";
-export type MWPageParseResponse = ApiParseParams & { categories?: { "*": string }[]; properties?: { [key: string]: string } };
+
+export type MWParsePageResponse = Partial<{
+  title: string;
+  pageid: number;
+  revid: number;
+  text: { "*": string };
+  categories: Array<{ sortkey: string; "*": string }>;
+  links: Array<{ ns: number; exists: string; "*": string }>;
+  templates: Array<{ ns: number; exists: string; "*": string }>;
+  images: string[];
+  externallinks: string[];
+  sections: Array<{
+    toclevel: number;
+    level: string;
+    line: string;
+    number: string;
+    index: string;
+    fromtitle: string;
+    byteoffset: number;
+    anchor: string;
+    linkAnchor: string;
+  }>;
+  displaytitle: string;
+  iwlinks: Array<{
+    prefix: string;
+    url: string;
+    "*": string;
+  }>;
+  properties: Array<{
+    name: string;
+    "*": string;
+  }>;
+}>;
